33d5b333e1e7d81515ee5e600080613a702e28f7
bbuseunsall <108673294+bbuseunsall@users.noreply.github.com>
2022-09-07 15:35:15 +0300

    Update ödev2_1.sql

--- ödev2_1.sql
+++ ödev2_1.sql
@@ -1,5 +1,5 @@
 SELECT * FROM film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99; 
 
-- Patika Proje1: [Patika] (https://app.patika.dev/courses/sql/Odev2)
+- Patika Ödev2: [Patika] (https://app.patika.dev/courses/sql/Odev2)
 

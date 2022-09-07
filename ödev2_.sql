6ba2400bc5064a0f32dfa0c533243a6d9395a76e
bbuseunsall <108673294+bbuseunsall@users.noreply.github.com>
2022-09-07 16:39:50 +0300

    Update and rename ödev2_1.sql to ödev2_.sql

---
+++ ödev2_.sql
@@ -0,0 +1,18 @@
+# Ödev 2, 1. Soru
+
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99; 
+
+# Ödev 2, 2. Soru
+
+SELECT first_name,last_name FROM actor
+WHERE first_name IN ('Penelope' ,'Nick','Ed');
+
+# Ödev 2, 3. Soru
+
+SELECT * FROM film
+WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
+
+
+- Patika Ödev2: [Patika] (https://app.patika.dev/courses/sql/Odev2)
+
--- ödev2_1.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM film
-WHERE replacement_cost BETWEEN 12.99 AND 16.99; 
-
-- Patika Ödev2: [Patika] (https://app.patika.dev/courses/sql/Odev2)
-

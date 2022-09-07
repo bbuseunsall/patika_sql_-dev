2ce500bb50e55f11b7b320bcb9f3b3a7279bfe4a
bbuseunsall <108673294+bbuseunsall@users.noreply.github.com>
2022-09-07 16:47:20 +0300

    Update and rename ödev4_1.sql to ödev4_.sql

---
+++ ödev4_.sql
@@ -0,0 +1,25 @@
+# Ödev 4, 1. Soru
+
+SELECT DISTINCT replacement_cost FROM film;
+
+# Ödev 4, 2. Soru
+
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+
+# Ödev 4, 3. Soru
+
+SELECT COUNT (title) FROM film
+WHERE title LIKE 'T%' AND rating='G';
+
+# Ödev 4, 4. Soru
+
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
+
+# Ödev 4, 5. Soru
+
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%r';
+
+
+- Patika Ödev4: [Patika] (https://app.patika.dev/courses/sql/Odev4)

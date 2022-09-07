36fe78df4dbc816ec3e56218bd1f08bebc77ecdd
bbuseunsall <108673294+bbuseunsall@users.noreply.github.com>
2022-09-07 16:44:18 +0300

    Update and rename ödev3_1.sql to ödev3_.sql

---
+++ ödev3_.sql
@@ -0,0 +1,17 @@
+# Ödev 3, 1. Soru
+
+SELECT country FROM country 
+WHERE country LIKE  'A%a';
+
+# Ödev 3, 2. Soru
+
+SELECT country FROM country 
+WHERE country LIKE '_____%n';
+
+# Ödev 3, 4. Soru
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
+
+
+- Patika Ödev3: [Patika] (https://app.patika.dev/courses/sql/Odev3)
--- ödev3_1.sql
+++
@@ -1,4 +0,0 @@
-SELECT country FROM country 
-WHERE country LIKE  'A%a';
-
-- Patika Ödev3: [Patika] (https://app.patika.dev/courses/sql/Odev3)

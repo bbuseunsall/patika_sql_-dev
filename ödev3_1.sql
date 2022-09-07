fb59f94a83007464ba81ae7850e0c2af79c1d7ac
bbuseunsall <108673294+bbuseunsall@users.noreply.github.com>
2022-09-07 15:49:09 +0300

    Update ödev3_1.sql

--- ödev3_1.sql
+++ ödev3_1.sql
@@ -1,4 +1,4 @@
 SELECT country FROM country 
 WHERE country LIKE  'A%a';
 
-- Patika Ödev2: [Patika] (https://app.patika.dev/courses/sql/Odev3)
+- Patika Ödev3: [Patika] (https://app.patika.dev/courses/sql/Odev3)
